87a6b069d4f840b7fa12ecd54e0066cede6f9dca
johan101429 <jona1014.mr@gmail.com>
2022-03-19 09:38:09 -0500

    for y while

--- ciclos/ciclos.js
+++ ciclos/ciclos.js
@@ -1,9 +1,15 @@
-var numeros = 20;
-for (var i = 1; i <= 20; i++) {
+let numeros = 20;
+for (let i = 1; i <= 20; i++) {
     if (i % 2 == 1) {
         document.write("impar");
 
+
+    } else if (i % 2 == 0) {
+        document.write("par");
     }
+
+
+
     document.write(i + "<br/>")
 
 }
